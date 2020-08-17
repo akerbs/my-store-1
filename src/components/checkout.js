605344ca644748d2ede155b25ac025e958d9e903
Anatol Kerbs <anker2702@gmail.com>
2020-08-17 13:38:18 +0200

    initial

---
+++ src/components/checkout.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react"
+import { loadStripe } from "@stripe/stripe-js"
+
+const buttonStyles = {
+  fontSize: "13px",
+  textAlign: "center",
+  color: "#000",
+  padding: "12px 60px",
+  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
+  backgroundColor: "rgb(255, 178, 56)",
+  borderRadius: "6px",
+  letterSpacing: "1.5px",
+}
+
+const buttonDisabledStyles = {
+  opacity: "0.5",
+  cursor: "not-allowed",
+}
+
+let stripePromise
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      "pk_test_51HGUuRHwITO0GSJr0YK6FwbE17LUTst9UCvm2uH0RdjBtAnQJqgPmDn0BSunRc8FIEXRW3HatsFd1uDHkfaGJtUm00IA2780Iw"
+    )
+    // stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+  }
+  return stripePromise
+}
+
+const Checkout = () => {
+  const [loading, setLoading] = useState(false)
+
+  const redirectToCheckout = async event => {
+    event.preventDefault()
+    setLoading(true)
+
+    const stripe = await getStripe()
+    const { error } = await stripe.redirectToCheckout({
+      mode: "payment",
+      lineItems: [{ price: "price_1HGjcwHwITO0GSJrJEhUG0Aq", quantity: 1 }],
+      // lineItems: [{ price: process.env.GATSBY_BUTTON_PRICE_ID, quantity: 1 }],
+      successUrl: `http://localhost:8000/page-2/`,
+      cancelUrl: `http://localhost:8000/`,
+    })
+
+    if (error) {
+      console.warn("Error:", error)
+      setLoading(false)
+    }
+  }
+
+  return (
+    <button
+      disabled={loading}
+      style={
+        loading ? { ...buttonStyles, ...buttonDisabledStyles } : buttonStyles
+      }
+      onClick={redirectToCheckout}
+    >
+      BUY MY BOOK
+    </button>
+  )
+}
+
+export default Checkout
